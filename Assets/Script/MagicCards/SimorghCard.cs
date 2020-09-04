44a2ea9ece69c617da5df844affe86d5974f41d6
Soroor <aghamiri.soroor@gmail.com>
2020-09-04 14:56:37 +0430

    Info panel with new art

---
+++ Assets/Script/MagicCards/SimorghCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private WinChecker winChecker;
+    private bool levelIsOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winChecker = GameObject.Find("WinChecker").GetComponent<WinChecker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelIsOver = winChecker.flagWin;
+    }
+}
