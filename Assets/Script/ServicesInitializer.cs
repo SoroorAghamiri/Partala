a9a0f7e8f856669b97ac1dbd12848296751c3ea3
Falric <tgkthefirst@gmail.com>
2020-09-14 17:24:19 +0430

    Adding Tapsell

--- Assets/Script/ServicesInitializer.cs
+++ Assets/Script/ServicesInitializer.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameAnalyticsSDK;
 using BazaarInAppBilling;
+using TapsellSDK;
 
 public class ServicesInitializer : MonoBehaviour
 {
+    [SerializeField] string TAPSELL_KEY;
     private void Awake()
     {
         StoreHandler.instance.InitializeBillingService();
+        Tapsell.Initialize(TAPSELL_KEY);
         GameAnalytics.Initialize();
+
     }
     // Start is called before the first frame update
     void Start()
