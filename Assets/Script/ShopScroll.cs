4f9d0de903ebb47ff779cd3f17e434e4ddccd59d
sorooraghamiri <aghamiri.soroor@gmail.com>
2020-12-18 11:30:45 +0330

    Shop is completed

--- Assets/Script/ShopScroll.cs
+++ Assets/Script/ShopScroll.cs
@@ -8,18 +8,12 @@ public class ShopScroll : MonoBehaviour
 {
     public List<GameObject> hiddenOffers;
     GameObject currentButton;
-    // public void testButton(){
-    //     Debug.Log("Button clicked");
-    // }
-    public void moreButtonClicked(){
-        currentButton = EventSystem.current.currentSelectedGameObject;
-        iTween.ScaleTo(currentButton , iTween.Hash("scale" ,Vector3.zero , "time",  0.7f , "ignoretimescale" , true));
-        // for(int i = 0 ; i < hiddenOffers.Count ; i++){
-        //     // hiddenOffers[i].SetActive(true);
-        //      iTween.ScaleTo(hiddenOffers[i] ,iTween.Hash("scale" ,Vector3.one , "time",  1f , "ignoretimescale" , true) );
-        // }
-        // currentButton.SetActive(false);
 
+    public void moreButtonClicked(){
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+        currentButton = clickedButton.transform.parent.gameObject;
+        iTween.ScaleTo(currentButton , iTween.Hash("scale" ,Vector3.zero , "time",  0.7f , "ignoretimescale" , true));
+    
         StartCoroutine(enableHiddens());
         
     }
@@ -29,9 +23,9 @@ public class ShopScroll : MonoBehaviour
         Debug.Log("First ienum");
         currentButton.SetActive(false);
         for(int i = 0 ; i < hiddenOffers.Count ; i++){
-            // hiddenOffers[i].SetActive(true);
+            
             iTween.ScaleTo(hiddenOffers[i] ,iTween.Hash("scale" ,new Vector3(1.3f , 1.3f , 1.3f) , "time", 1f , "ignoretimescale" , true));//new Vector3(1.3f , 1.3f , 1.3f)
-            // iTween.ScaleTo(hiddenOffers[i] ,iTween.Hash("scale" ,Vector3.one , "time",  1f , "ignoretimescale" , true) );
+           
         }
         StartCoroutine(resizeHiddens());
     }
@@ -39,8 +33,7 @@ public class ShopScroll : MonoBehaviour
         yield return new WaitForSecondsRealtime(0.3f);
         Debug.Log("Second ienum");
          for(int i = 0 ; i < hiddenOffers.Count ; i++){
-            // hiddenOffers[i].SetActive(true);
-            // iTween.ScaleTo(hiddenOffers[i] ,iTween.Hash("scale" ,new Vector3(1.3f , 1.3f , 1.3f) , "time", 1f , "ignoretimescale" , true));//new Vector3(1.3f , 1.3f , 1.3f)
+          
             iTween.ScaleTo(hiddenOffers[i] ,iTween.Hash("scale" ,Vector3.one , "time",  1f , "ignoretimescale" , true) );
         }
     }
