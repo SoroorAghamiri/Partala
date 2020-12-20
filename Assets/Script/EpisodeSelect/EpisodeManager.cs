7c87a10b3f8757eeebcd228333af27f835705c27
Falric <tgkthefirst@gmail.com>
2020-12-20 22:09:33 +0330

    enabling episodes

--- Assets/Script/EpisodeSelect/EpisodeManager.cs
+++ Assets/Script/EpisodeSelect/EpisodeManager.cs
@@ -26,7 +26,14 @@ public class EpisodeManager : MonoBehaviour
         {
             DataManager.Instance.SetEpisode(1);
         }
-
+        for(int i=0;i<episodeButtons.Length;i++)
+        {
+            episodeButtons[i].interactable = false;
+        }
+        for(int i=0;i<DataManager.Instance.GetEpisode();i++)
+        {
+            episodeButtons[i].interactable = true;
+        }
     }
 
     // Update is called once per frame
