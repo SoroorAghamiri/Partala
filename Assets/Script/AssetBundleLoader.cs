c8d0758dc5b2747712b1b9cee4761aea6a18c4ae
Falric <tgkthefirst@gmail.com>
2021-01-18 18:37:19 +0330

    testing WWW class for download and caching asset bundles

--- Assets/Script/AssetBundleLoader.cs
+++ Assets/Script/AssetBundleLoader.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 
 public class AssetBundleLoader : MonoBehaviour
 {
-    [SerializeField] AssetBundle assetBundle;
+    public string BundleURL;
+    public string AssetName;
+    public int version;
+
     // Start is called before the first frame update
-    private void Awake()
+    //private void Awake()
+    //{
+    //    var myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/episode2level16");
+    //    if (myLoadedAssetBundle == null)
+    //    {
+    //        Debug.Log("Failed to load AssetBundle!");
+    //    }
+    //    else
+    //    {
+    //        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Episode2.Level16");
+    //        Instantiate(prefab);
+    //    }
+    //}
+    private void Start()
     {
-        var myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/episode2level16");
-        if (myLoadedAssetBundle == null)
+        StartCoroutine(DownloadAndCache());
+    }
+    IEnumerator DownloadAndCache()
+    {
+        // Wait for the Caching system to be ready
+        while (!Caching.ready)
+            yield return null;
+
+        // Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
+        
+        using (WWW www = WWW.LoadFromCacheOrDownload(BundleURL, version))
         {
-            Debug.Log("Failed to load AssetBundle!");
-        }
-        else
-        {
-            var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Episode2.Level16");
-            Instantiate(prefab);
-        }
+            yield return www;
+            if (www.error != null)
+            {
+                Debug.Log("WWW download had an error:" + www.error);
+            }
+                
+            AssetBundle bundle = www.assetBundle;
+            //if (AssetName == "")
+            //    Instantiate(bundle.mainAsset);
+            //else
+            var prefab = bundle.LoadAsset<GameObject>("Episode2.Level16");
+                Instantiate(prefab,transform);
+            // Unload the AssetBundles compressed contents to conserve memory
+            bundle.Unload(false);
+
+        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
     }
 }
