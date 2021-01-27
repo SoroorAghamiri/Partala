be14ba7c2ae562df8b35d03466c8dd9e40a4ef39
Falric <tgkthefirst@gmail.com>
2021-01-27 17:19:51 +0330

    Adding A Basic Code For AssetBundle

--- Assets/Script/AssetBundleLoader.cs
+++ Assets/Script/AssetBundleLoader.cs
@@ -6,74 +6,40 @@ using UnityEngine.Networking;
 
 public class AssetBundleLoader : MonoBehaviour
 {
-    public string BundleURL;
-    public string AssetName;
-    public string hashCode;
-    public uint crc;
+
+    [SerializeField] string nameOfPrefab;
+
 
     private GameManger myGameManger;
     private WinCheckerMoreThan2Objects winchecker;
     private TouchManager touchManager;
 
-    // Start is called before the first frame update
-    //private void Awake()
-    //{
-    //    var myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/episode2level16");
-    //    if (myLoadedAssetBundle == null)
-    //    {
-    //        Debug.Log("Failed to load AssetBundle!");
-    //    }
-    //    else
-    //    {
-    //        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Episode2.Level16");
-    //        Instantiate(prefab);
-    //    }
-    //}
+
     private void Start()
     {
         myGameManger = FindObjectOfType<GameManger>();
         winchecker = FindObjectOfType<WinCheckerMoreThan2Objects>();
         touchManager = FindObjectOfType<TouchManager>();
-        StartCoroutine(DownloadAndCache());
+        GetAssetBundleAndLoadLevel();
     }
-    IEnumerator DownloadAndCache()
+    private void GetAssetBundleAndLoadLevel()
     {
-        CachedAssetBundle cachedAssetBundle = new CachedAssetBundle();
-        Hash128 hash = Hash128.Parse(hashCode);
 
-        cachedAssetBundle.hash = hash;
-        cachedAssetBundle.name = AssetName;
-        // Wait for the Caching system to be ready
-        while (!Caching.ready)
-            yield return null;
+        // Get downloaded asset bundle
+        AssetBundle bundle = DataManager.Instance.GetAssetBundle();
+        var prefab = bundle.LoadAsset<GameObject>(nameOfPrefab);
 
-        // Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
-        using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(BundleURL, cachedAssetBundle, crc))
-        {
-            yield return uwr.SendWebRequest();
+        Instantiate(prefab, transform.position, Quaternion.identity, transform);//Fix This Later
 
-            if (uwr.isNetworkError || uwr.isHttpError)
-            {
-                Debug.Log(uwr.error);
-            }
-            else
-            {
-                // Get downloaded asset bundle
-                AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
-                var prefab = bundle.LoadAsset<GameObject>("Episode2.Level16");
+        CallScripts();
+        //Implement Calling Other Scripts
 
-                Instantiate(prefab, transform.position, Quaternion.identity, transform);
-
-                CallScripts();
-                //Implement Calling Other Scripts
-            }
-        }
     }
 
     private void CallScripts()
     {
         myGameManger.GetComponent<GameManger>().enabled = true;
-        
+
         touchManager.GetComponent<TouchManager>().ABActive = true;
         touchManager.GetComponent<TouchManager>().enabled = true;
 
